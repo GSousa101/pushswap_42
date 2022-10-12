61cea2bec00a2219af2936608566e4ea1c062c66
gusousa <gfsousa42@gmail.com>
2022-10-12 10:39:08 -0300

    header of new functions

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 15:58:43 by gusousa           #+#    #+#             */
-/*   Updated: 2022/10/06 16:00:14 by gusousa          ###   ########.fr       */
+/*   Updated: 2022/10/12 10:38:50 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,20 @@ typedef struct	s_stack
 	int				content;
 	struct s_stack	*next;
 }	t_stack;
+
+void	sa(t_list **lst_a);
+void	sb(t_list **lst_b);
+void	ss(t_list **lst_a, t_list **lst_b);
+
+void	pa(t_list **lst_b, t_list **lst_a);
+void	pb(t_list **lst_a, t_list **lst_b);
+
+void	ra(t_list **lst_a);
+void	rb(t_list **lst_b);
+void	rr(t_list **lst_a, t_list **lst_b);
+
+void	rra(t_list **lst_a);
+void	rrb(t_list **lst_b);
+void	rrr(t_list **lst_a, t_list **lst_b);
 
 #endif
