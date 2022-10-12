7fffe0483e2e425b6fb7e989e86ee61a51945736
gusousa <gfsousa42@gmail.com>
2022-10-12 16:31:26 -0300

    new swap function

---
+++ src/swap.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 14:43:37 by gusousa           #+#    #+#             */
+/*   Updated: 2022/10/12 16:30:15 by gusousa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.c"
+
+void	swap(t_list **lst)
+{
+	t_list	*aux;
+	t_list	*second_node;
+
+	//My idea
+	second_node = *lst->next;
+	if (second_node)
+	{
+		*lst->next = *second_node->next;
+		second_node->next = *lst;
+		*lst = second_node;
+	}
+}
+
+void	sa(t_list **lst_a)
+{
+	swap(lst_a);
+	ft_printf("sa\n");
+}
+
+void	sb(t_list **lst_b)
+{
+	swap(lst_b);
+	ft_printf("sb\n");
+}
+
+void	ss(t_list **lst_a, t_list **lst_b)
+{
+	swap(lst_a);
+	swap(lst_b);
+	ft_printf("ss\n");
+}
+
