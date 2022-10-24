8418a62fde4da5f27e79976f92589800b95fc403
gusousa <gfsousa42@gmail.com>
2022-10-24 17:08:06 -0300

    new is_ordered

--- src/sort_small.c
+++ src/sort_small.c
@@ -6,15 +6,26 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 15:13:16 by gusousa           #+#    #+#             */
-/*   Updated: 2022/10/24 16:22:49 by gusousa          ###   ########.fr       */
+/*   Updated: 2022/10/24 17:07:48 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+int	is_ordered(t_list *list_a)
+{
+	while (list_a->next)
+	{
+		if (ft_atoi(list_a->content) > ft_atoi(list_a->next->content))
+			return (0);
+		list_a = list_a->next;
+	}
+	return (1);
+}
+
 void	sort_3(t_list **list_a)
 {
-	while (!is_ordered)
+	while (!is_ordered(*list_a))
 	{
 		if (biggest_front(list_a) || smaller_front(list_a))
 		{
@@ -29,7 +40,7 @@ void	sort_3(t_list **list_a)
 void	sort_5(t_list **list_a)
 {
 
-	while (!is_ordered)
+	while (!is_ordered(*list_a))
 	{
 		// Organiza 2
 		sort_3();
