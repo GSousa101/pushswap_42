fbf5d98801abb2fd698827fe9ffc77ca6dfb5c6c
gusousa <gfsousa42@gmail.com>
2022-10-24 16:23:06 -0300

    Back bone of small

--- src/sort_small.c
+++ src/sort_small.c
@@ -6,7 +6,7 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 15:13:16 by gusousa           #+#    #+#             */
-/*   Updated: 2022/10/24 15:14:13 by gusousa          ###   ########.fr       */
+/*   Updated: 2022/10/24 16:22:49 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 void	sort_3(t_list **list_a)
 {
-	(void)list_a;
+	while (!is_ordered)
+	{
+		if (biggest_front(list_a) || smaller_front(list_a))
+		{
+			ra(list_a);
+			sa(list_a);
+		}
+		else
+			ra(list_a);
+	}
+}
+
+void	sort_5(t_list **list_a)
+{
+
+	while (!is_ordered)
+	{
+		// Organiza 2
+		sort_3();
+	}
 }
